003085ee91803451c052c9414808b81ebacc5fa5
Mümin Köykıran <muminkoykiran@gmail.com>
2018-09-07 15:39:20 +0300

    Başlangıçta çalışma için geliştirmeler
    Başlangıçta çalışma için geliştirmeler eklendi

--- setup.sh
+++ setup.sh
@@ -81,7 +81,9 @@ cp rgbControlClass.py $ARTEX_PATH
 cd ../../..
 
 #apt-get install libasound2-dev memcached python-pip python-alsaaudio vlc -y
-cp initd_artex.sh /etc/init.d/ArtexPi
+#cp initd_artex.sh /etc/init.d/ArtexPi
+cp initd_artex.sh /usr/local/bin/Artex
+sudo chmod +x /usr/local/bin/Artex
 #update-rc.d ArtexPi defaults
 cp $ARTEX_PATH/resources/ArtexPi.service /usr/lib/systemd/user/
 systemctl --user enable ArtexPi.service
